5308dcd3a4b4b9f5ed2b96b78193a84dcb03ed51
345000346 <345000346@qq.com>
2024-12-28 06:57:32 +0800

    删除：移除maupassant主题中的多个不必要文件和部分代码

--- themes/maupassant/static/js/addaTartget.js
+++
@@ -1,8 +0,0 @@
-//给 <a> 标签批量加上 `tartget = "_blank"
-function addaTarget(id) {
-    var aTags = document.getElementById(id).getElementsByTagName("a");
-    for (i = 0; i < aTags.length; i++) {
-        var aTags_item = aTags[i];
-        aTags_item.target = "_blank";
-    }
-}
